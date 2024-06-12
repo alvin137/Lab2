8696111e3db76588106e2f078db571237b19e5c1
alvin137 <alvin1377@gmail.com>
2024-06-12 21:49:16 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,26 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
 
+    if (vec.size() == M) {
+        for (auto e: vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
 
-
-
-
-
-
+    for (int i = cnt + 1; i <= N; i++) {
+        vec.emplace_back(i);
+        func(i);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
